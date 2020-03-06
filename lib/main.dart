df9e2c7175bff2d70885df3e7b3768964eb2206b
bincy <bincypaul1997@gmail.com>
2020-03-06 18:30:22 +0530

    customfab

--- lib/main.dart
+++ lib/main.dart
@@ -3,10 +3,322 @@ import 'package:flutter/material.dart';
 void main() => runApp(
   MaterialApp(
     title: 'MyApp',
-    home: MyScaffold(),
+    home: _tutorialhome(),
     debugShowCheckedModeBanner: false,
   )
 );
+
+class _Customfab extends StatefulWidget{
+
+  final Function() onPressed;
+  final String tooltip;
+  final IconData icon;
+
+  _Customfab({this.onPressed, this.tooltip, this.icon});
+  @override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    return Customfab();
+  }
+}
+
+class Customfab extends State<_Customfab> with SingleTickerProviderStateMixin{
+
+  bool isOpened = false;
+  AnimationController _animationController;
+  Animation<Color> _buttonColor;
+  Animation<double> _animateIcon;
+  Animation<double> _translateButton;
+  Curve _curve = Curves.easeOut;
+  double _fabHeight = 56.0;
+
+  @override
+  initState() {
+    _animationController =
+    AnimationController(vsync: this, duration: Duration(milliseconds: 500))
+      ..addListener(() {
+        setState(() {});
+      });
+    _animateIcon =
+        Tween<double>(begin: 0.0, end: 1.0).animate(_animationController);
+    _buttonColor = ColorTween(
+      begin: Colors.blue,
+      end: Colors.red,
+    ).animate(CurvedAnimation(
+      parent: _animationController,
+      curve: Interval(
+        0.00,
+        1.00,
+        curve: Curves.linear,
+      ),
+    ));
+    _translateButton = Tween<double>(
+      begin: _fabHeight,
+      end: -14.0,
+    ).animate(CurvedAnimation(
+      parent: _animationController,
+      curve: Interval(
+        0.0,
+        0.75,
+        curve: _curve,
+      ),
+    ));
+    super.initState();
+  }
+
+
+  @override
+  dispose() {
+    _animationController.dispose();
+    super.dispose();
+  }
+
+  animate() {
+    if (!isOpened) {
+      _animationController.forward();
+    } else {
+      _animationController.reverse();
+    }
+    isOpened = !isOpened;
+  }
+
+  Widget add() {
+    return Container(
+      child: FloatingActionButton(
+        onPressed: null,
+        tooltip: 'Add',
+        child: Icon(Icons.add),
+      ),
+    );
+  }
+
+  Widget image() {
+    return Container(
+      child: FloatingActionButton(
+        onPressed: null,
+        tooltip: 'Image',
+        child: Icon(Icons.image),
+      ),
+    );
+  }
+
+  Widget inbox() {
+    return Container(
+      child: FloatingActionButton(
+        onPressed: null,
+        tooltip: 'Inbox',
+        child: Icon(Icons.inbox),
+      ),
+    );
+  }
+
+  Widget toggle() {
+    return Container(
+      child: FloatingActionButton(
+        backgroundColor: _buttonColor.value,
+        onPressed: animate,
+        tooltip: 'Toggle',
+        child: AnimatedIcon(
+          icon: AnimatedIcons.menu_close,
+          progress: _animateIcon,
+        ),
+      ),
+    );
+  }
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.end,
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: <Widget>[
+        Transform(
+          transform: Matrix4.translationValues(
+            0.0,
+            _translateButton.value * 3.0,
+            0.0,
+          ),
+          child: add(),
+        ),
+        Transform(
+          transform: Matrix4.translationValues(
+            0.0,
+            _translateButton.value * 2.0,
+            0.0,
+          ),
+          child: image(),
+        ),
+        Transform(
+          transform: Matrix4.translationValues(
+            0.0,
+            _translateButton.value,
+            0.0,
+          ),
+          child: inbox(),
+        ),
+        toggle(),
+      ],
+    );
+  }
+}
+
+class _tutorialhome extends StatefulWidget{
+
+
+  @override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    return TutorialHome();
+  }
+}
+
+class TutorialHome extends State<_tutorialhome> with SingleTickerProviderStateMixin{
+  bool isOpened = false;
+  AnimationController _animationController;
+  Animation<Color> _animateColor;
+  Animation<double> _animateIcon;
+  Curve _curve = Curves.easeOut;
+  Animation<Color> _buttonColor;
+  Animation<double> _translateButton;
+  double _fabHeight = 56.0;
+  int selectedindex=0;
+
+  void OnItemTapped (int index){
+    setState(() {
+      selectedindex = index;
+    });
+  }
+
+  @override
+  initState() {
+    _animationController =
+    AnimationController(vsync: this, duration: Duration(milliseconds: 500))
+      ..addListener(() {
+        setState(() {});
+      });
+
+    _animateIcon =
+        Tween<double>(begin: 0.0, end: 1.0).animate(_animationController);
+
+    _buttonColor = ColorTween(
+      begin: Colors.blue,
+      end: Colors.red,
+    ).animate(CurvedAnimation(
+      parent: _animationController,
+      curve: Interval(
+        0.00,
+        1.00,
+        curve: Curves.linear,
+      ),
+    ));
+
+    _translateButton = Tween<double>(
+      begin: _fabHeight,
+      end: -14.0,
+    ).animate(CurvedAnimation(
+      parent: _animationController,
+      curve: Interval(
+        0.0,
+        0.75,
+        curve: _curve,
+      ),
+    ));
+
+    _animateColor = ColorTween(
+      begin: Colors.blue,
+      end: Colors.red,
+    ).animate(CurvedAnimation(
+      parent: _animationController,
+      curve: Interval(
+        0.00,
+        1.00,
+        curve: _curve,
+      ),
+    ));
+    super.initState();
+  }
+
+  @override
+  dispose() {
+    _animationController.dispose();
+    super.dispose();
+  }
+
+  animate(){
+      if(!isOpened){
+          _animationController.forward();
+        }
+      else{
+        _animationController.reverse();
+       }
+      isOpened = !isOpened;
+  }
+
+  Widget add() {
+    return Container(
+      child: FloatingActionButton(
+        onPressed: null,
+        tooltip: 'Add',
+        child: Icon(Icons.add),
+      ),
+    );
+  }
+
+  Widget image() {
+    return Container(
+      child: FloatingActionButton(
+        onPressed: null,
+        tooltip: 'Image',
+        child: Icon(Icons.image),
+      ),
+    );
+  }
+
+  Widget inbox() {
+    return Container(
+      child: FloatingActionButton(
+        onPressed: null,
+        tooltip: 'Inbox',
+        child: Icon(Icons.inbox),
+      ),
+    );
+  }
+
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Scaffold(
+      appBar: AppBar(
+        leading: IconButton(icon: Icon(Icons.menu), onPressed: null),
+        title: Text("MyApp"),
+        actions: <Widget>[
+          IconButton(icon: Icon(Icons.search), onPressed: null)
+        ],
+      ),
+      body: _Customfab(),
+//      floatingActionButton: FloatingActionButton(
+//        backgroundColor: _animateColor.value,
+//        onPressed: animate,
+//        tooltip: 'Toggle',
+//        child: AnimatedIcon(
+//          icon: AnimatedIcons.menu_close,
+//          progress: _animateIcon,
+//        ),
+//      ),
+      bottomNavigationBar: BottomNavigationBar(
+          items: const <BottomNavigationBarItem>
+          [
+            BottomNavigationBarItem(icon: Icon(Icons.home),title: Text("Home")),
+            BottomNavigationBarItem(icon: Icon(Icons.data_usage), title: Text("Analysis")),
+            BottomNavigationBarItem(icon: Icon(Icons.person), title: Text("Profile"))
+          ],
+          currentIndex: selectedindex,
+          onTap: OnItemTapped,
+       ),
+    );
+  }
+}
 
 class MyAppbar extends StatelessWidget{
   final Widget title;
